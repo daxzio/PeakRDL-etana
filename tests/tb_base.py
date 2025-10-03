0afab0901461cafaae2a98a8dd83472a77ca7604
Dave Keeshan <dave.keeshan@daxzio.com>
2025-10-03 13:59:08 +0100

    Add axi suppoert

--- tests/tb_base.py
+++ tests/tb_base.py
@@ -1,11 +1,5 @@
-from cocotbext.apb import ApbBus
-from cocotbext.apb import ApbMaster
-
 from interfaces.clkreset import Clk
 from interfaces.clkreset import Reset
-
-from interfaces.passthrough import PTBus, PTMaster
-from interfaces.axi_driver import AxiDriver
 
 
 class testbench:
@@ -16,22 +10,24 @@ class testbench:
         self.reset = Reset(dut, self.clk, reset_sense=reset_sense, resetname="rst")
 
         if hasattr(dut, "s_cpuif_req"):
+            from interfaces.passthrough import PTBus, PTMaster
+
             pt_bus = PTBus.from_prefix(dut, "s_cpuif")
             self.intf = PTMaster(pt_bus, getattr(dut, "clk"))
         elif hasattr(dut, "s_apb_penable"):
+            from cocotbext.apb import ApbBus
+            from cocotbext.apb import ApbMaster
+
             apb_bus = ApbBus.from_prefix(dut, "s_apb")
             self.intf = ApbMaster(apb_bus, getattr(dut, "clk"))
         elif hasattr(dut, "s_axil_awvalid"):
+            from interfaces.axi_driver import AxiDriver
+
             apb_bus = ApbBus.from_prefix(dut, "s_apb")
             self.intf = AxiDriver(dut, "s_axil", "clk")
         else:
             raise Exception("Unsupported interface")
 
-        #         exit()
-        #         print(dir(dut))
-        #         if hasattr(dut, 'hwif_out'):
-        #             print(dir(dut.hwif_out))
-        #         else:
         for attr in dir(dut):
             if attr.startswith("hwif_"):
                 sig = getattr(dut, attr)
@@ -39,7 +35,3 @@ class testbench:
                 # Only initialize inputs, not outputs
                 if attr.startswith("hwif_in_"):
                     sig.value = 0
-                # print(f"setattr(self, {attr}, {sig})")
-
-
-#         exit()
