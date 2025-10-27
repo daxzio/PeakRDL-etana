94d7e4a7f077d5ca09eff5fd988d83536aa2627d
Dave Keeshan <dave.keeshan@daxzio.com>
2025-10-27 23:30:33 +0000

    Remove tests-debug, no longer required

--- tests-debug/rtlflo/compile_libs.tcl
+++
@@ -1,5 +0,0 @@
-set ver $env(XILINX_REV)
-set compile_dir "xilinx_lib/${ver}"
-file mkdir ${compile_dir}
-puts "${compile_dir}"
-compile_simlib -simulator modelsim -directory ${compile_dir}
--- tests-debug/test_dmi/html_dir/search/bkt_index.js
+++
@@ -1,1 +0,0 @@
-var SearchBucketIndex = [262860213];
